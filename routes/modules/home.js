ca5f3a3ecea84021d85b674686dac501b2eba54f
Wayne <urrightguy@gmail.com>
2021-06-12 22:33:34 +0800

    feat: set RESTful API and Express.Router

---
+++ routes/modules/home.js
@@ -0,0 +1,17 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+// 引用 Todo model
+const restaurant = require('../../models/restaurant')
+
+// route setting with models seeder connection
+router.get('/', (req, res) => {
+  restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+// 匯出路由模組
+module.exports = router
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -1,51 +1,18 @@
-// require package used in the project
+// 引用 Express 與 Express 路由器
 const express = require('express')
-const app = express()
-const port = 3000
+const router = express.Router()
 
-// read models seeder
-const restaurant = require('./models/restaurant')
-
-// set express handlebars(exphbs)
-const exphbs = require('express-handlebars')
-app.engine('handlebars', exphbs({ defaultlayout: 'main' }))
-app.set('view engine', 'handlebars')
-
-// set Mongoose
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-const db = mongoose.connection       // 取得資料庫連線狀態
-db.on('error', () => {               // 連線異常
-  console.log('mongodb error!')
-})
-db.once('open', () => {              // 連線成功
-  console.log('mongodb connected!')
-})
-
-// setting body-parser
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// setting static files
-app.use(express.static('public'))
-
-// setting method-override
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
-
-// 引用 routes
-const routes = require('./routes')
-app.use(routes)
+// 引用 Todo model
+const restaurant = require('../../models/restaurant')
 
 
 // route setting for create new restaurant
-app.get('/restaurants/new', (req, res) => {
+router.get('/restaurants/new', (req, res) => {
   return res.render('new')
 })
 
 // route setting for catch created restaurant
-app.post('/restaurants', (req, res) => {
+router.post('/restaurants', (req, res) => {
   const name = req.body.name
   const name_en = req.body.name_en
   const category = req.body.category
@@ -62,7 +29,7 @@ app.post('/restaurants', (req, res) => {
 })
 
 // route setting for show page
-app.get('/restaurants/:id', (req, res) => {
+router.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return restaurant.findById(id)
     .lean()
@@ -71,7 +38,7 @@ app.get('/restaurants/:id', (req, res) => {
 })
 
 // route setting for getting edit function
-app.get('/restaurants/:id/edit', (req, res) => {
+router.get('/restaurants/:id/edit', (req, res) => {
   const id = req.params.id
   return restaurant.findById(id)
     .lean()
@@ -80,7 +47,7 @@ app.get('/restaurants/:id/edit', (req, res) => {
 })
 
 // route setting for posting edit function
-app.put('/restaurants/:id', (req, res) => {
+router.put('/restaurants/:id', (req, res) => {
   const id = req.params.id
   const name = req.body.name
   const name_en = req.body.name_en
@@ -111,7 +78,7 @@ app.put('/restaurants/:id', (req, res) => {
 })
 
 // route setting for deletion
-app.delete('/restaurants/:id', (req, res) => {
+router.delete('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return restaurant.findById(id)
     .then(restaurant => restaurant.remove())
@@ -120,7 +87,7 @@ app.delete('/restaurants/:id', (req, res) => {
 })
 
 // route setting for search not yet
-app.get('/search', (req, res) => {
+router.get('/search', (req, res) => {
   const keyword = req.query.keyword.toLowerCase().trim()
 
   return restaurant.find()
@@ -134,7 +101,5 @@ app.get('/search', (req, res) => {
     .catch(error => console.log(error))
 })
 
-//start and listen on the Express server
-app.listen(port, () => {
-})
-
+// 匯出路由模組
+module.exports = router

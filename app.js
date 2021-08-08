8e870accb1bb0a608d4e1ad398bac8b2afa7d4b2
Wayne <urrightguy@gmail.com>
2021-08-08 23:41:56 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hbshelpers = require('handlebars-helpers')  // set handlebars-helpers
 const helpers = hbshelpers()
 const bodyParser = require('body-parser')          // setting body-parser
 const methodOverride = require('method-override')  // setting method-override
+const flash = require('connect-flash')
+
 const routes = require('./routes')                 // 引用 routes
 const usePassport = require('./config/passport')
 
@@ -24,6 +26,15 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+
+app.use(flash())  // 掛載套件
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
 
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
